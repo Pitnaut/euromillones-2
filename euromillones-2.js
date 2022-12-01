14460df3cb29a4f3141c4ade3b69f0371caba34f
Pedro Arévalo <pitarevalo@gmail.com>
2022-12-01 13:17:46 +0100

    Complete add 5 numbers in an array

--- euromillones-2.js
+++ euromillones-2.js
@@ -1,9 +1,17 @@
+//RNG in a range
 const randomNumber = (minRange, maxRange) => {
     return Math.floor(Math.random() * (maxRange - minRange + 1) + minRange);
 };
 
-console.log(randomNumber(1, 50))
+//console.log(randomNumber(1, 50))
 
-let lottoNumbers = [];
+let getLottoNumbers = [];
 let i = 0;
+// numbers added in lottoNumbers array
+do {
+    i = randomNumber(1, 50);
+    getLottoNumbers.push(i);
+} while (getLottoNumbers.length < 5);
 
+
+console.log(getLottoNumbers)
