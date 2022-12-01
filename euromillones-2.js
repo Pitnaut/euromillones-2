cee03ab8936832f488f66b9a65d1279d4d63b055
Pedro Arévalo <pitarevalo@gmail.com>
2022-12-01 14:28:59 +0100

    Complete 2 star array

--- euromillones-2.js
+++ euromillones-2.js
@@ -3,15 +3,27 @@ const randomNumber = (minRange, maxRange) => {
     return Math.floor(Math.random() * (maxRange - minRange + 1) + minRange);
 };
 
-//console.log(randomNumber(1, 50))
-
+// numbers added in order in lottoNumbers array
 let lottoNumbers = [];
 let i = 0;
-// numbers added in order in lottoNumbers array
+
 do {
     i = randomNumber(1, 50);
     lottoNumbers.push(i);
-    lottoNumbers.sort(function(a, b) {return a - b})
+    lottoNumbers.sort(function(a, b) {return a - b});
 } while (lottoNumbers.length < 5);
 
+//numbers added in order in starNumbers array
+let starNumbers = []
+let j = 0;
+
+do {
+    j = randomNumber(1, 12);
+    starNumbers.push(j);
+    starNumbers.sort(function(a, b) {return a - b});
+} while (starNumbers.length < 2);
+
+
+
 console.log(lottoNumbers)
+console.log(starNumbers)
